2ad776f0a365ee513ccea3c8bdb5785d0b8f4615
hlin <gywjd5126@gmail.com>
2023-01-13 15:34:42 +0900

    User Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,20 @@
+import { authService } from "myFirebase";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-  return <span>Profile</span>;
+  const navigation = useNavigate();
+
+  // Make user log out
+  const onLogOut = () => {
+    authService.signOut();
+    navigation("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOut}>Log Out</button>
+    </>
+  );
 }
 export default Profile;
